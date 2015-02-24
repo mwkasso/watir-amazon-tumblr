ef27cb6650a9cb6ab3116a496404a9383f489e6a
Mohammed Kasso <mkasso@spartaglobal.com>
2015-02-24 15:23:12 +0000

    Reduced number of lines on tumblr.rb

--- tumblr.rb
+++ tumblr.rb
@@ -2,19 +2,13 @@ require "watir"
 
 browser = Watir::Browser.new :firefox
 browser.goto "https://www.tumblr.com"
-l = browser.link :href, "https://www.tumblr.com/login"
-l.click
-email = browser.text_field id: "signup_email"
-email.set "larry.jonesmrfakeffakelarry-jones2015@hotmail.com"
-pass = browser.text_field id: "signup_password"
-pass.set "BzZsjXjUTpIcq0w"
+browser.link(:href, "https://www.tumblr.com/login").click
+browser.text_field(id: "signup_email").set "larry.jonesmrfakeffakelarry-jones2015@hotmail.com"
+browser.text_field(id: "signup_password").set "BzZsjXjUTpIcq0w"
 browser.button(id: "signup_forms_submit").click
-l = browser.link href: "/new/text?redirect_to=%2Fdashboard"
-l.click
-titlebox = browser.div class: "editor-plaintext"
-titlebox.send_keys "This is a test"
-textbox = browser.div class: "editor-richtext"
-textbox.send_keys "This is a test"
+browser.link(href: "/new/text?redirect_to=%2Fdashboard").click
+browser.div(class: "editor-plaintext").send_keys "This is a test"
+browser.div(class: "editor-richtext").send_keys "This is a test"
 browser.button(class:"create_post_button").click
 browser.button(class:"create_post_button").wait_while_present
 browser.div(class:"post_title").wait_until_present
@@ -24,4 +18,3 @@ if browser.h2(class:"title").text == "This is a test"
 else
   puts "Failed to make post"
 end
-
